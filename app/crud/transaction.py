d782aaa1d64fe4eef78d0e063b42391a85173dff
Agamya28 <agamya.verma28@gmail.com>
2026-06-13 00:02:58 +0530

    Add transaction authorization and ownership validation

--- app/crud/transaction.py
+++ app/crud/transaction.py
@@ -4,14 +4,14 @@ from app.database.models import Transaction
 from app.schemas.transaction import TransactionCreate
 
 
-def create_transaction(db: Session, transaction: TransactionCreate):
+def create_transaction(db: Session, transaction: TransactionCreate, user_id: int):
     db_transaction = Transaction(
         amount=transaction.amount,
         category=transaction.category,
         merchant=transaction.merchant,
         description=transaction.description,
         transaction_date=transaction.transaction_date,
-        user_id=transaction.user_id
+        user_id=user_id
     )
 
     db.add(db_transaction)
@@ -20,13 +20,16 @@ def create_transaction(db: Session, transaction: TransactionCreate):
 
     return db_transaction
 
-def get_transactions(db: Session)-> list[Transaction]:
-    return db.query(Transaction).all()
+def get_transactions(db: Session, user_id: int)-> list[Transaction]:
+    return db.query(Transaction).filter(Transaction.user_id==user_id).all()
 
-def delete_transaction(db: Session, transaction_id: int):
+def delete_transaction(db: Session, transaction_id: int, user_id: int):
     transaction = (
         db.query(Transaction)
-        .filter(Transaction.id == transaction_id)
+        .filter(
+            Transaction.id == transaction_id , 
+            Transaction.user_id ==user_id
+            )
         .first()
     )
 
--- app/routers/transactions.py
+++ app/routers/transactions.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
+from app.dependencies.auth import get_current_user
 
 from app.database.database import get_db
 from app.schemas.transaction import (
@@ -20,22 +21,25 @@ router = APIRouter(
 @router.post("/", response_model=TransactionResponse)
 def create_transaction_route(
     transaction: TransactionCreate,
-    db: Session = Depends(get_db)
+    db: Session = Depends(get_db),
+    current_user= Depends(get_current_user)
 ):
-    return create_transaction(db, transaction)
+    return create_transaction(db=db, transaction=transaction, user_id=current_user.id)
 
 @router.get("/", response_model=list[TransactionResponse])
 def get_transactions_route(
-    db: Session = Depends(get_db)
+    db: Session = Depends(get_db),
+    current_user= Depends(get_current_user)
 ):
-    return get_transactions(db)
+    return get_transactions(db=db, user_id=current_user.id)
 
 @router.delete("/{transaction_id}")
 def delete_transaction_route(
     transaction_id: int,
-    db: Session = Depends(get_db)
+    db: Session = Depends(get_db),
+    current_user= Depends(get_current_user)
 ):
-    transaction = delete_transaction(db, transaction_id)
+    transaction = delete_transaction(db=db, transaction_id=transaction_id, user_id=current_user.id)
 
     if transaction is None:
         raise HTTPException(

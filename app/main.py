2caf92d994378743f6024a16f3549de4ac2e0716
Agamya28 <agamya.verma28@gmail.com>
2026-06-29 13:00:52 +0530

    Fix CORS for vercel frontend

--- app/main.py
+++ app/main.py
@@ -16,6 +16,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
+        "https://ai-financial-copilot-phi.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
